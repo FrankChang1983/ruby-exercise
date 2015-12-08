a897f71abe5e8ef40c923150e030e207710e802c
SHOUTSE <frankchang1983@gmail.com>
2015-12-08 18:31:29 +0800

    add ex1.rb

---
+++ arrays/ex1.rb
@@ -0,0 +1,7 @@
+puts "enter a number"
+number = gets.chomp.to_i
+arr = [1, 3, 5, 7, 9, 11]
+if arr.include?(number)
+  puts "#{number} is indeed in the array."
+elsif puts "#{number} is not in the array"
+end
